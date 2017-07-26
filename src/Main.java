1ca1ecb91578a0f33e2bfc098453ec7608173232
BenjaminHb <79770999@qq.com>
2017-07-26 20:17:55 +0800

    Version 1.5.5.1707262018
    Optimized SmilesToPubChemFIngerprint.java

---
+++ src/Main.java
@@ -0,0 +1,68 @@
+import java.io.*;
+
+/**
+ * Created by benjaminzhang on 12/07/2017.
+ * Copyright © benjaminzhang 2017.
+ */
+public class Main {
+    private String readFileName = new String("/Users/benjaminzhang/Downloads/Smile3.txt");
+    private String writeFileName = new String("/Users/benjaminzhang/Downloads/PubChemFingerprint2.txt");
+    private StringBuffer stringBuffer = new StringBuffer();
+
+    public void Run(){
+        File file = new File(readFileName);
+        Reader reader = null;
+
+        try {
+            //System.out.println("以字符为单位读取文件内容，一次读一个字节：");
+            // 一次读一个字符
+            reader = new InputStreamReader(new FileInputStream(file));
+            int tempchar;
+            int singleQuoteMark = 0;
+            writeFileFromFileEnd("[");
+            while ((tempchar = reader.read()) != -1) {
+                // 对于windows下，rn这两个字符在一起时，表示一个换行。但如果这两个字符分开显示时，会换两次行。
+                // 因此，屏蔽掉r，或者屏蔽n。否则，将会多出很多空行。
+                if (((char) tempchar) != 'r' && (char) tempchar != ',' && (char) tempchar != ' ') {
+                    System.out.print("Reading...");
+                    if ((char) tempchar == '\'')    singleQuoteMark++;
+                    else    stringBuffer.append((char) tempchar);
+                    if (singleQuoteMark == 2) {
+                        singleQuoteMark = 0;
+                        SmilesToPubChemFingerprint smilesToPubChemFingerprint = new SmilesToPubChemFingerprint(stringBuffer.toString());
+                        writeFileFromFileEnd(smilesToPubChemFingerprint.getFingerprintBitSetString()+", ");
+                        stringBuffer.delete(0,stringBuffer.length());
+                        System.out.println("\nreadFileStatus = true");
+                    }
+                }
+            }
+            reader.close();
+            writeFileFromFileEnd("]");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 追加文件尾部：使用RandomAccessFile
+     */
+    protected void writeFileFromFileEnd(String content) {
+        try {
+            // 打开一个随机访问文件流，按读写方式
+            RandomAccessFile randomFile = new RandomAccessFile(writeFileName, "rw");
+            // 文件长度，字节数
+            long fileLength = randomFile.length();
+            // 将写文件指针移到文件尾
+            randomFile.seek(fileLength);
+            randomFile.writeBytes(content);
+            randomFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.Run();
+    }
+}
---
+++ src/SmilesToPubChemFingerprint.java
@@ -0,0 +1,65 @@
+import org.openscience.cdk.DefaultChemObjectBuilder;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.exception.InvalidSmilesException;
+import org.openscience.cdk.fingerprint.IBitFingerprint;
+import org.openscience.cdk.fingerprint.PubchemFingerprinter;
+import org.openscience.cdk.interfaces.*;
+import org.openscience.cdk.silent.SilentChemObjectBuilder;
+import org.openscience.cdk.smiles.SmilesParser;
+
+import java.util.BitSet;
+
+/**
+ * Created by benjaminzhang on 12/07/2017.
+ * Copyright © benjaminzhang 2017.
+ */
+public class SmilesToPubChemFingerprint {
+    private PubchemFingerprinter fprinter = new PubchemFingerprinter(DefaultChemObjectBuilder.getInstance());
+    private IBitFingerprint fingerprint;
+
+    protected SmilesToPubChemFingerprint(String smiles){
+        try {
+            SmilesParser smilesParser = new SmilesParser(SilentChemObjectBuilder.getInstance());
+            IAtomContainer atomContainer = smilesParser.parseSmiles(smiles);
+            fingerprint = fprinter.getBitFingerprint(atomContainer);
+        } catch (InvalidSmilesException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        } catch (CDKException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    protected BitSet getFingerprintBitSet(){
+        return fingerprint.asBitSet();
+    }
+
+    protected String getFingerprintBitSetString(){
+        StringBuffer stringBuffer = new StringBuffer();
+        int[] setBitArray = fingerprint.getSetbits();
+        stringBuffer.append('[');
+        for (int i = 0; i < setBitArray.length; i++) {
+            stringBuffer.append(setBitArray[i]);
+            if (i + 1 !=setBitArray.length) stringBuffer.append(", ");
+        }
+        stringBuffer.append(']');
+        return stringBuffer.toString();
+    }
+
+    protected String getFingerPrintBitString(){
+        StringBuffer stringBuffer = new StringBuffer();
+        int[] setBitArray = fingerprint.getSetbits();
+        int setBitArrayIndex = 0;
+
+        for (int i = 0; i < fprinter.getSize(); i++) {
+            if (setBitArrayIndex < setBitArray.length && setBitArray[setBitArrayIndex] == i) {
+                stringBuffer.append('1');
+                setBitArrayIndex++;
+            }
+            else stringBuffer.append('0');
+        }
+        return stringBuffer.toString();
+    }
+}
